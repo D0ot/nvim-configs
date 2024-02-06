a580e0044f890fe8ae51aab6f71176c833a925f4
doot <gugegby@gmail.com>
2024-02-06 20:19:32 +0800

    add(orgmode): add orgmode support

---
+++ ftplugin/org.lua
@@ -0,0 +1,7 @@
+local bo = vim.bo
+local cmd = vim.cmd
+
+bo.expandtab = false
+bo.shiftwidth = 2
+bo.tabstop = 2
+bo.softtabstop = 2
--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -5,4 +5,6 @@ require("orgmode").setup_ts_grammar()
 require("orgmode").setup({
 	org_agenda_files = "~/orgfiles/**/*",
 	org_default_notes_file = "~/orgfiles/refile.org",
+
+	org_todo_keywords = { "TODO", "DOING", "|", "DONE", "NOTE", "AGAIN" },
 })
