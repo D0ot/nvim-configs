179faf05b03f339fe19955ab543876a854fe7f40
Baoyuan Geng <geng.baoyuan@gmail.com>
2026-04-25 20:48:10 +0800

    fix: broken treesitter configs

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,46 +1,27 @@
 vim.g.cursorhold_updatetime = 100
 
-require("nvim-treesitter.install").prefer_git = true
-require("nvim-treesitter.configs").setup({
-	modules = {},
-	sync_install = false,
-	ignore_install = {},
-	auto_install = false,
-
-	ensure_installed = {
-		"c",
-		"cpp",
-		"javascript",
-		"rust",
-		"typescript",
-		"css",
-		"html",
-		"vue",
-		"lua",
-		"markdown",
-		"markdown_inline",
-		"luadoc",
-		"vimdoc",
-		"regex",
-	},
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = true,
-	},
-	refactor = {
-		navigation = {
-			enable = true,
-			keymaps = {
-				goto_next_usage = "<a-j>",
-				goto_previous_usage = "<a-k>",
-			},
-		},
-		highlight_current_scope = { enable = false },
-		highlight_definitions = {
-			enable = false,
-			-- Set to false if you have an `updatetime` of ~100.
-			-- TODO: It has performance issue, open linux/mm/page_alloc.c,
-			-- cursor will moves slowly...
-		},
-	},
+require("nvim-treesitter").setup({
+	-- Directory to install parsers and queries to (prepended to `runtimepath` to have priority)
+	install_dir = vim.fn.stdpath("data") .. "/site",
 })
+
+local ensure_installed = {
+	"c",
+	"cpp",
+	"javascript",
+	"rust",
+	"typescript",
+	"css",
+	"html",
+	"vue",
+	"lua",
+	"markdown",
+	"markdown_inline",
+	"luadoc",
+	"vimdoc",
+	"regex",
+}
+
+require("nvim-treesitter").install(ensure_installed)
+
+
